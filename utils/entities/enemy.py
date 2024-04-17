41ecc7b1f92fcac790d353df4bc64860ae5220e2
thanapatk <22460@tun.ac.th>
2024-04-17 16:49:03 +0700

    refactor: refactor entity

--- utils/entities/enemy.py
+++ utils/entities/enemy.py
@@ -1,4 +1,5 @@
 from enum import Enum
+
 from utils.entities.entity import Entity, Facing
 from utils.sprite import Sprites
 
@@ -15,25 +16,18 @@ class EnemyAction(Enum):
 
 class Enemy(Entity):
     def __init__(self, sprites: dict[str, Sprites], loc: tuple[int, int], facing: Facing) -> None:
-        super().__init__(sprites)
         self.current_action = EnemyAction.WALK
-        self.facing = facing
-        self.loc = loc
 
-        current_sprite = self.get_sprite()
-        self.prev_sprite = current_sprite
-        current_sprite.rect.x, current_sprite.rect.y = self.loc
+        super().__init__(sprites, loc, facing)
 
-        self.counter = 0
-
-    def update(self):
+    def update(self) -> None:
         match self.current_action:
             case EnemyAction.WALK:
                 self.a = 0
                 self.v = 2
             case EnemyAction.RUN:
                 self.a = 0
-                self.v = 5
+                self.v = 6
             case EnemyAction.KNOCK_BACK:
                 # TODO: Enemy Knock back
                 pass
@@ -42,13 +36,13 @@ class Enemy(Entity):
                 self.a = 0
                 self.v = 0
 
-                self.frame_time = self.get_sprite().sprites_count
+                frame_time = self.get_sprite().sprites_count
 
-                if self.counter == self.frame_time:
+                if self._counter == frame_time:
                     self.current_action = EnemyAction.WALK
-                    self.counter = 0
+                    self._counter = 0
                 else:
-                    self.counter += 1
+                    self._counter += 1
 
         current_sprite = self.get_sprite()
         current_sprite.flipped = self.facing == Facing.RIGHT
@@ -70,6 +64,3 @@ class Enemy(Entity):
                 sprite_name = self.current_action.value
 
         return self.sprites[sprite_name]
-
-    def get_all_sprites(self):
-        return self.sprites.values()
--- utils/entities/entity.py
+++ utils/entities/entity.py
@@ -1,20 +1,33 @@
-from _collections_abc import dict_values
 from abc import abstractmethod
-from enum import Enum, auto
-from pygame.transform import flip
+from enum import Enum
 
 from utils.sprite import Sprites
-
-
-class Entity:
-    def __init__(self, sprites: dict[str, Sprites]) -> None:
-        self.sprites = sprites
-
-    @abstractmethod
-    def update(self) -> None:
-        pass
 
 
 class Facing(Enum):
     LEFT = 1
     RIGHT = -1
+
+
+class Entity:
+    def __init__(self, sprites: dict[str, Sprites], loc: tuple[int, int], facing: Facing) -> None:
+        self.sprites = sprites
+        self.facing = facing
+        self.loc = loc
+
+        current_sprite = self.get_sprite()
+        self.prev_sprite = current_sprite
+        current_sprite.rect.x, current_sprite.rect.y = self.loc
+
+        self._counter = 0
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_sprite(self) -> Sprites:
+        pass
+
+    def get_all_sprites(self):
+        return self.sprites.values()
